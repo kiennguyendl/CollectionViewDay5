386145e7beee82f00ad7340d93478872537bf38d
Kien Nguyen Dang <kiennguyendang@Kiens-Mac.local>
2016-12-29 14:51:19 +0700

    first commit

---
+++ CollectionViewDay5/SubView.swift
@@ -0,0 +1,9 @@
+//
+//  SubView.swift
+//  CollectionViewDay5
+//
+//  Created by Tung Nguyen on 11/2/16.
+//  Copyright © 2016 Tung Nguyen. All rights reserved.
+//
+
+import Foundation
---
+++ README.md
@@ -0,0 +1,1 @@
+# CollectionViewDay5
